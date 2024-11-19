d6a6c9c206566b3fe9f17d96f5e31feff1172eed
DLogan99 <157902636+DLogan99@users.noreply.github.com>
2024-11-19 12:40:08 -0500

    Saving comments to remember where I am at.

---
+++ src/main/kotlin/Prime Factorization.kt
@@ -0,0 +1,12 @@
+//Have the user enter a number and find all Prime Factors (if there are any) and display them.
+// DOING IT FUNCTIONALLY
+
+fun main (args: Array<String>) {
+	println("Enter a number and this program will find all the prime factors if there are any.\n")
+	val n = readLine()?.toInt()
+
+}
+
+fun primes (n: Int) {
+
+}

207593f6c6ed8a51d69bf09471a5d3c0f16d9d1f
DLogan99 <157902636+DLogan99@users.noreply.github.com>
2024-11-23 10:11:09 -0500

    First build, prior any runs/builds.

--- src/main/kotlin/Prime Factorization.kt
+++ src/main/kotlin/Prime Factorization.kt
@@ -2,11 +2,51 @@
 // DOING IT FUNCTIONALLY
 
 fun main (args: Array<String>) {
-	println("Enter a number and this program will find all the prime factors if there are any.\n")
-	val n = readLine()?.toInt()
+
+	val find: Int = callInt()
+
+	val fFac: Int = findPrime(find)
+	val sFac: Int = findPrime(find/fFac)
+
+	if (fFac==0||sFac==0) {
+		println("Interal Error, please run the program again.")
+		return
+	}
+	else {
+		println("The factors of $find are $fFac and $sFac")
+	}
 
 }
 
-fun primes (n: Int) {
+fun callInt (): Int {
+	println("Enter a number and this program will find all the prime factors if there are any.\n")
+	val n = readLine()?.toInt()!!
+	if (isPrime(n)) {
+		println("That number is already a prime, the factors are 1 and $n.")
+		return callInt()
+	}
+	return n
+}
 
+fun findPrime (n: Int): Int {
+
+	for (i in 2 until n){
+		if (n%i == 0) {
+			if (isPrime(i)) return i
+			else if (isPrime(n/i)) return n/i
+			else  return findPrime(i)
+		}
+	}
+
+	return 0
+}
+
+fun isPrime(n: Int): Boolean {
+	if (n <= 2) return true
+
+	for (i in 2..n) {
+		if(n%i == 0) return false
+	}
+
+	return true
 }

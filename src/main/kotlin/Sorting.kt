bbf84a2c70ac61ea00ad07f5baedc9ad5f23ecfd
DLogan99 <157902636+DLogan99@users.noreply.github.com>
2024-11-26 22:59:59 +0100

    MergeSort run into some trouble. Done for the day, putting up for rest.

--- src/main/kotlin/Sorting.kt
+++ src/main/kotlin/Sorting.kt
@@ -22,9 +22,31 @@ fun bubbleSort (n: MutableList<Int>): MutableList<Int> {
 	else bubbleSort(finalList)
 }
 
-fun mergeSort (n: MutableList<Int>): IntArray {
+fun mergeSort (n: MutableList<Int>): MutableList<Int> {
+	//! Current problem is that it splits the entire array into arrays of 2 ([[1,5], [6,2]]), so after the first sort it would look like this ([[1,2], [5,6]]) - and would try to sort the same breakdown over and over again. Need to loop then reinitialize nest with half again and sort, then loop then reinitialize and sort again, over and over until the array.size = 1.
 
-	val nest: Array<IntArray> =
+	val nest: Array<IntArray> = Array(n.size/2) { IntArray(2) }
+
+	if (isSorted(n.toMutableList()))
+
+	if (n.size%2 != 1) {
+		for ( i in 0 until n.lastIndex/2 ) {
+			when ( n[i] > n[i+1] ) {
+				true -> nest[i] = intArrayOf()
+				false -> nest[i] = intArrayOf()
+			}
+			nest[i] = intArrayOf(n[i], n[i+1])
+		}
+	} else {
+		for ( i in 0 until n.lastIndex/2 ) {
+			if ( i+1 in 0..n.lastIndex) nest[i] = {
+
+			}
+			else nest[i] = intArrayOf(n[i])
+		}
+	}
+
+	return mergeSort(nest.flatt)
 }
 
 fun isSorted ( n: MutableList<Int> ): Boolean {

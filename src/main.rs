5e49e61310a128e92b88f199ad39e1db06938b2f
DLogan99 <157902636+DLogan99@users.noreply.github.com>
2024-12-14 11:56:44 +0100

    Finished - Stage 1 ready.

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,34 @@
 use std::io;
 
-fn main() {
-    let mut input: String = String::new();
-    let mut prime: u32 = 0;
-
-    println!("This program will generate Prime numbers up to the number you entered.");
-
-    let limit: u32 = loop {
-        io::stdin().read_line(&mut input).expect("Failed stdin.read_line.");
-        match input.trim().parse::<u32>() {
-            Ok(num) => {
-                break num;
-            }
-            Err(..) => {
-                println!("Input is not an integer: {}", input);
-                input.clear();
-                println!("Enter a fucking number dipshit.\n");
-            }
-        }
-    };
-
-    while limit > prime {
-        prime = pnp(prime + 1);
-        println!("prime: {}", prime);
-    }
+fn main () {
+	println!("This program will find PI to the nth digit, specified by you.");
+	loop {
+		println!("Please specify how many digits you want.");
+		let limit: i32 = {
+			let mut s = String::new();
+			io::stdin().read_line(&mut s).unwrap();
+			match s.trim().parse::<i32>() {
+				Ok(n) => n,
+				Err(e) => {
+					println!("Bad input, exiting program now. Error: {}", e);
+					std::process::exit(0);
+				}
+			}
+		};
+		println!("Your PI: {}", nth_digit(limit));
+		println!("Another? Y or N");
+		let mut input = String::new();
+		io::stdin().read_line(&mut input).unwrap();
+		if input.trim().to_lowercase() == "y" { continue }
+		else { 
+			println!("Thank you!");
+			break 
+		}
+	}
 }
 
-fn pnp(prior: u32) -> u32 {
-    if is_prime(prior) {
-        prior
-    } else {
-        pnp(prior + 1)
-    }
+
+fn nth_digit (n: i32) -> f64 {
+	let mult: f64= 10.0_f64.powi(n-1);
+	((std::f64::consts::PI * mult) as i128) as f64 / mult
 }
-
-fn is_prime(n: u32) -> bool {
-    if n <= 2 {
-        return true;
-    }
-
-    for i in 2..n {
-        if n % i == 0 {
-            return false;
-        }
-    }
-
-    true
-}
-
-// fn print_type_of<T>(_: &T) {
-//     println!("{}", std::any::type_name::<T>());
-// }

150e81d27269129e8399c199990bc2f80892423f
logan <dlogan99@proton.me>
2024-12-24 15:58:07 -0500

    final attempt

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,23 @@ use std::io::stdin;
 
 fn main () {
 
-	let mut cur = 0;
-	let mut pri = 0;
+	let mut cur: u128 = 1;
+	let mut pri: u128 = 0;
 	let mut inp = String::new();
+	let mut iter: u16 = 1;
 
+	println!("Please input a positive number:");
 	stdin().read_line(&mut inp).unwrap();
 
-	let mut n = inp.trim().parse::<u32>().expect("not a number");
+	let mut n: u128 = inp.trim().parse::<u128>().expect("not a number");
+		println!("input number: {}", inp);
 
-	while n < i {
+	while n > cur {
 		println!("{}", cur);
 		cur = cur + pri;
 		pri = cur - pri;
+		// std::thread::sleep(std::time::Duration::from_secs(1));
+		iter += 1;
 	}
-
+	println!("Iterations: {}", iter);
 }

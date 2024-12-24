e860526848e2f25f9adea3935688968cbcf85f88
logan <dlogan99@proton.me>
2024-12-24 15:48:33 -0500

    1st attempt

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,20 @@
-use std::io;
+//Enter a number and have the program generate the Fibonacci sequence to that number or to the Nth number.
+use std::io::stdin;
 
-fn main() {
-    let mut input: String = String::new();
-    let mut prime: u32 = 0;
+fn main () {
 
-    println!("This program will generate Prime numbers up to the number you entered.");
+	let mut cur = 0;
+	let mut pri = 0;
+	let mut inp = String::new();
 
-    let limit: u32 = loop {
-        io::stdin().read_line(&mut input).expect("Failed stdin.read_line.");
-        match input.trim().parse::<u32>() {
-            Ok(num) => {
-                break num;
-            }
-            Err(..) => {
-                println!("Input is not an integer: {}", input);
-                input.clear();
-                println!("Enter a fucking number dipshit.\n");
-            }
-        }
-    };
+	stdin().read_line(&mut inp).unwrap();
 
-    while limit > prime {
-        prime = pnp(prime + 1);
-        println!("prime: {}", prime);
-    }
+	let mut n = inp.trim().parse::<u32>().expect("not a number");
+
+	while n < i {
+		println!("{}", cur);
+		cur = cur + pri;
+		pri = cur - pri;
+	}
+
 }
-
-fn pnp(prior: u32) -> u32 {
-    if is_prime(prior) {
-        prior
-    } else {
-        pnp(prior + 1)
-    }
-}
-
-fn is_prime(n: u32) -> bool {
-    if n <= 2 {
-        return true;
-    }
-
-    for i in 2..n {
-        if n % i == 0 {
-            return false;
-        }
-    }
-
-    true
-}
-
-// fn print_type_of<T>(_: &T) {
-//     println!("{}", std::any::type_name::<T>());
-// }
